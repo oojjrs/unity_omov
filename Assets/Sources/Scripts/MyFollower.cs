360dc48a726366e18f67141e0b5df80c19811f87
oojjrs <oojjrs@gmail.com>
2025-04-01 00:14:15 +0900

    초간단 Follower 로직 추가

---
+++ Assets/Sources/Scripts/MyFollower.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Sources.Scripts
+{
+    // 초간단 이동기
+    public class MyFollower : MonoBehaviour
+    {
+        private Coroutine Coroutine { get; set; }
+
+        public void MoveTo(Vector3 pos, float speed, Action onFinish = default)
+        {
+            if (speed == 0)
+                return;
+
+            if (Coroutine != default)
+                StopCoroutine(Coroutine);
+
+            Coroutine = StartCoroutine(Func());
+
+            IEnumerator Func()
+            {
+                var distance = (pos - transform.position).magnitude;
+                var estimated = distance / speed;
+                var time = Time.time;
+                var start = transform.position;
+                while (true)
+                {
+                    var t = Mathf.Clamp01((Time.time - time) / estimated);
+                    transform.position = Vector3.Lerp(start, pos, t);
+
+                    if (t >= 1)
+                        break;
+
+                    yield return default;
+                }
+
+                transform.position = pos;
+
+                Coroutine = default;
+
+                onFinish?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Sources/Test/Scripts/FollowerTest.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Sources.Scripts;
+using UnityEngine;
+
+namespace Assets.Sources.Test.Scripts
+{
+    [RequireComponent(typeof(MyFollower))]
+    public class FollowerTest : MonoBehaviour
+    {
+        private bool Toggle { get; set; }
+
+        private void Start()
+        {
+            GetComponent<MyFollower>().MoveTo(transform.position + Vector3.right * 10, 3, Func);
+
+            void Func()
+            {
+                if (Toggle)
+                    GetComponent<MyFollower>().MoveTo(transform.position + Vector3.right * 10, 3, Func);
+                else
+                    GetComponent<MyFollower>().MoveTo(transform.position + Vector3.left * 10, 3, Func);
+
+                Toggle = !Toggle;
+            }
+        }
+    }
+}
